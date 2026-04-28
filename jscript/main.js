729addb75634029dfc336a6c70c90fe857f3f4b7
Cameron Gray <cameron.gray@senecapolytechnic.ca>
2026-04-28 08:31:58 -0400

    Data Updated

--- jscript/main.js
+++ jscript/main.js
@@ -13,8 +13,8 @@
 //
 // Data Version: update this when the data file changes to ensure the data 
 // is reloaded on clients side
-const dv = "20260419.1";
-const lastUpdated = "2026-04-19";
+const dv = "20260428.1";
+const lastUpdated = "2026-04-28";
 var activeTerm = "2264Global";
 // Data File: CSV file containing the data to be used by the app (https://seneca-tools.github.io/ToolsGlobal/assets)
 const dataFileRoot = "https://seneca-tools.github.io/ToolsGlobal/assets/";
